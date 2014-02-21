b59d259dcc6428a26e1468468fcafeff7646174b
Jens Jap <jensjap@gmail.com>
2014-02-20 22:28:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kupo::Application.config.secret_key_base = '9350efe3eddc96b04b5c25750794641cddb135485f366baf0ad425da0d549659640e944d3d1e920fdb710100489779747610f3570f8265f197c54709bd766880'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kupo::Application.config.session_store :cookie_store, key: '_kupo_session'
