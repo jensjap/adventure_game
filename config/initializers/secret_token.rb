af92e6b8dafc62becf09ee169e273d5fbf7ee471
Jens Jap <jensjap@gmail.com>
2014-02-20 22:32:51 -0500

    updates Gemfile and adds secret_token.rb to generate secret token dynamically.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,22 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Kupo::Application.config.secret_key_base = '9350efe3eddc96b04b5c25750794641cddb135485f366baf0ad425da0d549659640e944d3d1e920fdb710100489779747610f3570f8265f197c54709bd766880'
+
+#Kupo::Application.config.secret_key_base = '9350efe3eddc96b04b5c25750794641cddb135485f366baf0ad425da0d549659640e944d3d1e920fdb710100489779747610f3570f8265f197c54709bd766880'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
