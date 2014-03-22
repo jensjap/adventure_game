704ce81b90e17f6144d2336db7ce48924e7cc2b1
Jens Jap <jensjap@gmail.com>
2014-03-22 19:38:36 -0400

    Makes a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
   subject { page }
-
   shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
     it { should have_title(full_title page_title)  }
