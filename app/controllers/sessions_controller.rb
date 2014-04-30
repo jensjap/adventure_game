490bc2c058af9490f3bf295c1bf8b51f92c11e1e
Jens Jap <jensjap@gmail.com>
2014-04-30 00:27:40 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)   # form_tag
     if user && user.authenticate(params[:password])       # form_tag
       sign_in user
-      redirect_to user, notice: "Logged in!"
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Jens Jap"
-    email    "jensjap@gmail.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@infalliblekitty.com" }
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
